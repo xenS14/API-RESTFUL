e5dda913c6a63bfb68f37bd4ea08a010ffcb1f65
Manuel Tarby <manuel.tarby@gmail.com>
2023-10-25 14:35:38 +0200

    Mise en place de l'architecture MVC

---
+++ controleur/apiRestful.py
@@ -0,0 +1,90 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+releves = [
+    {"id": 13873,
+     "ch": "545A004124240406020400000641884907900004150B0E173B0500000008AAC0000001A404B7001900020B62182233000E5600B8384406182660000E1300B7FF3C010A5EDC0D0A",
+     "date": "Thu, 25 Nov 2021 12:04:00 GMT"}
+]
+
+sonde = []
+
+
+class Releve(Resource):
+    def get(self, id):
+        for releve in releves:
+            if id == releve["id"]:
+                return releve, 200
+        return "Relevé introuvable", 404
+
+    def post(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("ch")
+        parser.add_argument("date")
+        args = parser.parse_args()
+
+        for releve in releves:
+            if id == releve["id"]:
+                return "Ce relevé {} existe déjà".format(id), 400
+
+        releve = {
+            "id": id,
+            "ch": args["ch"],
+            "date": args["date"]
+        }
+        releves.append(releve)
+        return releve, 201
+
+    def put(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("ch")
+        parser.add_argument("date")
+        args = parser.parse_args()
+
+        for releve in releves:
+            if (id == releve["id"]):
+                releve["ch"] = args["ch"]
+                releve["date"] = args["date"]
+                return releve, 200
+
+        releve = {
+            "id": id,
+            "ch": args["ch"],
+            "date": args["date"],
+        }
+        releves.append(releve)
+        return releve, 201
+
+
+class Sonde(Resource):
+    def get(self, id):
+        for sonde in sondes:
+            if id == sonde["id"]:
+                return sonde, 200
+        return "Sonde introuvable", 404
+
+    def post(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("ch")
+        parser.add_argument("date")
+        args = parser.parse_args()
+
+        for sonde in sondes:
+            if id == sonde["id"]:
+                return "Cette sonde {} existe déjà".format(id), 400
+
+        sonde = {
+            "id": id,
+            "ch": args["ch"],
+            "date": args["date"]
+        }
+        releves.append(sonde)
+        return sonde, 201
+
+
+api.add_resource(Releve, "/releve/<int:id>")
+api.add_resource(Sonde, "/sonde/<int:id>")
+app.run(debug=True)
