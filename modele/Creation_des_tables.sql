880d6370c2d7abb7484078c9270651ec452a120b
Manuel Tarby <manuel.tarby@gmail.com>
2023-12-01 10:08:52 +0100

    Modification de la BDD (ajout d'une relation entre table alerte et table sonde

--- modele/Creation_des_tables.sql
+++ modele/Creation_des_tables.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 01 déc. 2023 à 09:07
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -22,13 +31,14 @@ DROP TABLE IF EXISTS alerte;
 CREATE TABLE alerte (
   idAlerte int NOT NULL,
   Niv double DEFAULT NULL,
-  Operateur varchar(2) DEFAULT NULL,
-  Type varchar(45) DEFAULT NULL,
-  Actif tinyint DEFAULT NULL,
-  Utilisateur_idUtilisateur int NOT NULL DEFAULT '1',
-  frequence_envoi_mail varchar(45) DEFAULT NULL,
-  dernier_envoi datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  Operateur varchar(2) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Type varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Active tinyint DEFAULT NULL,
+  Utilisateur_idUtilisateur int NOT NULL,
+  frequence_envoi_mail varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  dernier_releve datetime DEFAULT NULL,
+  Sonde_idSonde varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -40,8 +50,7 @@ DROP TABLE IF EXISTS releve;
 CREATE TABLE releve (
   idReleve int NOT NULL,
   Date_releve datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -51,19 +60,19 @@ CREATE TABLE releve (
 
 DROP TABLE IF EXISTS sonde;
 CREATE TABLE sonde (
-  idSonde varchar(8) NOT NULL,
-  Nom varchar(45) DEFAULT NULL,
-  Active tinyint NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  idSonde varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  Nom varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Active tinyint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table sonde
 --
 
 INSERT INTO sonde (idSonde, Nom, Active) VALUES
-('06190485', '', 1),
-('62182233', '', 1),
-('62190434', '', 1);
+('06190485', 'Salon', 1),
+('62182233', 'Cour', 1),
+('62190434', 'Jardin', 1);
 
 -- --------------------------------------------------------
 
@@ -73,14 +82,13 @@ INSERT INTO sonde (idSonde, Nom, Active) VALUES
 
 DROP TABLE IF EXISTS sonde_has_releve;
 CREATE TABLE sonde_has_releve (
-  Sonde_idSonde varchar(8) NOT NULL,
+  Sonde_idSonde varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
   Releve_idReleve int NOT NULL,
   Temperature double DEFAULT NULL,
-  Humidite varchar(5) DEFAULT NULL,
+  Humidite varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   Niveau_batterie double DEFAULT NULL,
   Signal_RSSI int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -91,14 +99,14 @@ CREATE TABLE sonde_has_releve (
 DROP TABLE IF EXISTS utilisateur;
 CREATE TABLE utilisateur (
   idUtilisateur int NOT NULL,
-  Prenom varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  Nom varchar(45) DEFAULT NULL,
-  Numero_telephone varchar(13) DEFAULT NULL,
-  Email varchar(60) DEFAULT NULL,
-  Identifiant varchar(45) DEFAULT NULL,
-  Mot_de_passe varchar(45) DEFAULT NULL,
-  account_API varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  Prenom varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Nom varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Numero_telephone varchar(13) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Email varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Identifiant varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  Mot_de_passe varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  account_API varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table utilisateur
@@ -117,8 +125,8 @@ INSERT INTO utilisateur (idUtilisateur, Prenom, Nom, Numero_telephone, Email, Id
 ALTER TABLE alerte
   ADD PRIMARY KEY (idAlerte),
   ADD UNIQUE KEY idAlerte_UNIQUE (idAlerte),
-  ADD KEY fk_Alerte_Utilisateur1_idx (Utilisateur_idUtilisateur);
-
+  ADD KEY fk_Alerte_Utilisateur1_idx (Utilisateur_idUtilisateur),
+  ADD KEY fk_Alerte_Sonde1_idx (Sonde_idSonde);
 
 --
 -- Index pour la table releve
@@ -164,6 +172,24 @@ ALTER TABLE alerte
 --
 ALTER TABLE utilisateur
   MODIFY idUtilisateur int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table alerte
+--
+ALTER TABLE alerte
+  ADD CONSTRAINT fk_Alerte_Sonde1 FOREIGN KEY (Sonde_idSonde) REFERENCES sonde (idSonde),
+  ADD CONSTRAINT fk_Alerte_Utilisateur1 FOREIGN KEY (Utilisateur_idUtilisateur) REFERENCES utilisateur (idUtilisateur);
+
+--
+-- Contraintes pour la table sonde_has_releve
+--
+ALTER TABLE sonde_has_releve
+  ADD CONSTRAINT fk_Sonde_has_Releve_Releve1 FOREIGN KEY (Releve_idReleve) REFERENCES releve (idReleve),
+  ADD CONSTRAINT fk_Sonde_has_Releve_Sonde1 FOREIGN KEY (Sonde_idSonde) REFERENCES sonde (idSonde);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
